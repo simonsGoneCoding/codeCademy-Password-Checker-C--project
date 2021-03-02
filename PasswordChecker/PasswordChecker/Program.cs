c5d3735899842f2a60b9ac84d389df713fa1d423
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-02 22:46:42 +0000

    Complete all bulletpoints.

--- PasswordChecker/PasswordChecker/Program.cs
+++ PasswordChecker/PasswordChecker/Program.cs
@@ -6,11 +6,63 @@ namespace PasswordChecker
     {
         static void Main(string[] args)
         {
-            int minLength;
-            string uppercase;
-            string lowercase;
-            string digits;
-            string specialChars; 
+            // Setup 
+            int minLength = 8;
+            string uppercase = "ABCDEFGHJIKLMNOPQRSTUWXYZ";
+            string lowercase = "ABCDEFGHJIKLMNOPQRSTUWXYZ".ToLower();
+            string digits = "0123456789";
+            string specialChars = "~!@#$%^&*-_+";
+
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+
+            // Test 
+            int score = 0; 
+
+            if(password.Length >= 8)
+            {
+                score++;
+            }
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+            if (Tools.Contains(password, lowercase))
+            {
+                score++; 
+            }
+            if (Tools.Contains(password, digits))
+            {
+                score++; 
+            }
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+
+
+            // Inform the user
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("The password is extremely strong");
+                    break;
+                case 3:
+                    Console.WriteLine("The password is strong");
+                    break;
+                case 2:
+                    Console.WriteLine("The password is medium");
+                    break;
+                case 1:
+                    Console.WriteLine("The password is weak");
+                    break;
+                default:
+                    Console.WriteLine("The password doesn’t meet any of the standards");
+                    break;
+            }
+
 
         }
     }
