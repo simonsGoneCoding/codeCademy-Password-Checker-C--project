981efc889afa1648a5fb750d961d857391d23e05
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-02 22:16:27 +0000

    Initial commit - codeCademy - Password Checker - C# logic and conditionals project.

---
+++ PasswordChecker/PasswordChecker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minLength;
+            string uppercase;
+            string lowercase;
+            string digits;
+            string specialChars; 
+
+        }
+    }
+}
